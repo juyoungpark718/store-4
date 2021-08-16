54526c9a5a872e31c489cfb3811754d955ccee42
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-16 11:05:37 +0900

    test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello, plz...!awefawefaawefawefaweawefawef!!!asdf</div>, document.querySelector('#root'));
+ReactDOM.render(<div>Hello, plz...awefawef!awefawefaawefawefaweawefawef!!!asdf</div>, document.querySelector('#root'));
